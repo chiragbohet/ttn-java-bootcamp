1e70f27e32cd5c86941251f05000905dc6f6c41e
ChiragBohet <chirag.bohet@tothenew.com>
2020-02-19 09:56:27 +0530

    Added q6

--- java-1/intelliJ/java-1-assignment/src/q6/FindUniqueElement.java
+++ java-1/intelliJ/java-1-assignment/src/q6/FindUniqueElement.java
@@ -1,5 +1,21 @@
 package q6;
 
 public class FindUniqueElement {
-    
+
+  public static int getUnique(int[] arr) {
+
+    int unique = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      unique = unique ^ arr[i];
+    }
+
+    return unique;
+  }
+
+  public static void main(String[] args) {
+
+    int arr[] = {1, 1, 2, 2, 3};
+    System.out.println(getUnique(arr));
+  }
 }
