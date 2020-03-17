76081f40c64021066e2ba9b5c539f3559018eb1a
ChiragBohet <chirag.bohet@tothenew.com>
2020-03-17 13:45:41 +0530

    Added q11

--- Assignments/src/main/java/com/learning/day2_java2/q11/Driver.java
+++ Assignments/src/main/java/com/learning/day2_java2/q11/Driver.java
@@ -1,32 +1,71 @@
 package main.java.com.learning.day2_java2.q11;
 
-import java.util.HashMap;
+/*
+
+11. Convert the following code so that it uses nested while statements instead of for statements:
+        int s = 0;
+        int t = 1;
+        for (int i = 0; i < 10; i++)
+        {
+        s = s + i;
+        for (int j = i; j > 0; j−−)
+        {
+        t = t * (j - i);
+        }
+        s = s * t;
+        System.out.println("T is " + t);
+        }
+        System.out.println("S is " + s);
+
+*/
 
 public class Driver {
 
-   static void original() {
+  static void original() {
+    int s = 0;
+    int t = 1;
+    for (int i = 0; i < 10; i++) {
+      s = s + i;
+      for (int j = i; j > 0; j--) {
+        t = t * (j - i);
+      }
+      s = s * t;
+      System.out.println("T is " + t);
+    }
+    System.out.println("S is " + s);
+  }
 
-       int s = 0;
-       int t = 1;
+  static void converted() {
 
-       for (int i = 0; i < 10; i++)
-       {
-           s = s + i;
+    int s = 0;
+    int t = 1;
 
-           for (int j = i; j > 0; j--)
-           {
-               t = t * (j - i);
-           }
-           s = s * t;
-           System.out.println("T is " + t);
-       }
-       System.out.println("S is " + s);
+    int i = 0; // for 1 initialization
 
-       //throw new Exception("some exception!");
+    while (i < 10) // for 1 condition check
+    {
+      s = s + i;
+
+      int j = i; // for 2 initialization
+
+      while (j > 0) // for 2 condition check
+      {
+        t = t * (j - i);
+        j--; // for 2 decrement
+      }
+
+      s = s * t;
+
+      System.out.println("T is " + t);
+
+      i++; // for 1 increment
     }
 
-    public static void main (String[] args) {
-        System.out.println("Hello World!");
+    System.out.println("S is " + s);
+  }
 
-    }
+  public static void main(String[] args) {
+    //Driver.original();
+    Driver.converted();
+  }
 }
